7c12ff646be19193f1a2385cb1222dc3f7a4a6d7
Sangbum Kim <sangbum0123@naver.com>
2025-10-02 16:33:29 +0900

    Title: 비교 연산, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251002/비교 연산/comparison-operator.py
@@ -0,0 +1,36 @@
+a,b = map(int,input().split())
+
+if a >= b:
+    print("1")
+
+else:
+        print("0")
+
+if a > b:
+    print("1")
+else:
+        print("0")
+
+
+
+if a <= b:
+    print("1")
+
+else:
+        print("0")
+
+if a < b:
+    print("1")
+else:
+        print("0")
+
+
+if a == b:
+    print("1")
+else:
+        print("0")
+
+if a != b:
+    print("1")
+else:
+        print("0")
