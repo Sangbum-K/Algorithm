0c36111dc2bd17c333e0f8423ee278171c1c06ec
Sangbum Kim <sangbum0123@naver.com>
2025-10-09 17:16:37 +0900

    Title: 남녀노소 구분짓기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251009/남녀노소 구분짓기/sex-and-age.py
+++ 251009/남녀노소 구분짓기/sex-and-age.py
@@ -3,14 +3,14 @@ age = int(input())
 
 
 if gender == 0:
-    if age >= 18:
+    if age >= 19:
         print("MAN")
 
     else:
         print("BOY")
 
 else:
-    if age >= 18:
+    if age >= 19:
         print("WOMAN")
 
     else:
