4363f1690d89b0d3aa4ce9c7ae2800b5845f751b
Sangbum Kim <sangbum0123@naver.com>
2025-10-20 16:17:58 +0900

    Title: 학점 계산기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251020/학점 계산기/credit-calculator.py
@@ -0,0 +1,13 @@
+n = int(input())
+score = list(map(float,input().split()))
+
+grade = sum(score)/n
+
+print(f"{grade:.1f}")
+
+if grade >= 4.0:
+    print("Perfect")
+elif grade >= 3.0:
+    print("Good")
+else:
+    print("Poor")
