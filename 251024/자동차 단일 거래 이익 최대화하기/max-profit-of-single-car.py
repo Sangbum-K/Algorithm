f0f43f53e4b48d719d5719a48da1a7192ad79e28
Sangbum Kim <sangbum0123@naver.com>
2025-10-24 16:41:20 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251024/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int,input().split()))
+
+diff = 0
+
+for i in range(n):
+    for j in range(i,n):
+
+        if arr[j] - arr[i] > diff :
+            diff = arr[j] - arr[i]
+
+print(diff)
+
