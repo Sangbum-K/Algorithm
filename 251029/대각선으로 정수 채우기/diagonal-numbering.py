41a6b4e8adf043151596340ac306fe70934bfffd
Sangbum Kim <sangbum0123@naver.com>
2025-10-29 15:07:10 +0900

    Title: 대각선으로 정수 채우기, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251029/대각선으로 정수 채우기/diagonal-numbering.py
@@ -0,0 +1,73 @@
+n, m = map(int, input().split())
+
+arr = [[0 for j in range (m)] for i in range(n)]
+
+arr[0][0] = 1
+
+i,j = 0,1
+cnt = 2
+
+r = 1
+
+while 1:
+
+    arr[i][j] = cnt
+    
+
+    i += 1
+    j -= 1
+
+    cnt += 1
+
+    if j == 0 or i == n - 1:
+        arr[i][j] = cnt
+        i = 0
+        r += 1
+        j = r
+        cnt += 1
+
+        if r == m:
+            break
+    
+
+i = 1
+j = m-1
+d = 1
+
+while 1:
+    arr[i][j] = cnt
+
+    i += 1
+    j -= 1
+
+    cnt += 1
+
+
+    if j == 0 or i == n - 1:
+        arr[i][j] = cnt
+        j = m-1
+        d += 1
+        i = d
+        cnt += 1
+        
+        if i == n-1:
+            arr[n-1][m-1] = cnt
+            break
+
+
+
+
+
+
+for r in arr:
+    for c in r:
+        print(c, end = " ")
+    print()
+    
+    
+
+
+
+
+
+    
