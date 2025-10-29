72dd4075715428e7ab31c99d79e36f27fe376f27
Sangbum Kim <sangbum0123@naver.com>
2025-10-29 16:03:30 +0900

    Title: 파스칼의 삼각형, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251029/파스칼의 삼각형/pascals-triangle.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+arr = [[0 for j in range (n)] for i in range(n)]
+
+arr[0][0] = 1
+
+for i in range(1,n):
+    arr[i][0] = 1
+    for j in range(0,i+1):
+        if j >= 1:
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+
+for i in range(n):
+    for j in range(0,i+1):
+        print(arr[i][j], end = " ")
+    print()
+        
+        
