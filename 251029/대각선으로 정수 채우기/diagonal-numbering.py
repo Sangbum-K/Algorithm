a7395007e999512052abb114140273e7f82336b8
Sangbum Kim <sangbum0123@naver.com>
2025-10-29 15:09:16 +0900

    Title: 대각선으로 정수 채우기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251029/대각선으로 정수 채우기/diagonal-numbering.py
+++ 251029/대각선으로 정수 채우기/diagonal-numbering.py
@@ -9,50 +9,54 @@ cnt = 2
 
 r = 1
 
-while 1:
+if n>= 2 and m>= 2 :
+    while 1:
 
-    arr[i][j] = cnt
+        arr[i][j] = cnt
     
 
-    i += 1
-    j -= 1
+        i += 1
+        j -= 1
 
-    cnt += 1
-
-    if j == 0 or i == n - 1:
-        arr[i][j] = cnt
-        i = 0
-        r += 1
-        j = r
         cnt += 1
 
-        if r == m:
-            break
+        if j == 0 or i == n - 1:
+            arr[i][j] = cnt
+            i = 0
+            r += 1
+            j = r
+            cnt += 1
+
+            if r == m:
+                break
     
 
-i = 1
-j = m-1
-d = 1
+    i = 1
+    j = m-1
+    d = 1
 
-while 1:
-    arr[i][j] = cnt
-
-    i += 1
-    j -= 1
-
-    cnt += 1
-
-
-    if j == 0 or i == n - 1:
+    while 1:
         arr[i][j] = cnt
-        j = m-1
-        d += 1
-        i = d
+
+        i += 1
+        j -= 1
+
         cnt += 1
+
+
+        if j == 0 or i == n - 1:
+            arr[i][j] = cnt
+            j = m-1
+            d += 1
+            i = d
+            cnt += 1
         
-        if i == n-1:
-            arr[n-1][m-1] = cnt
-            break
+            if i == n-1:
+                arr[n-1][m-1] = cnt
+                break
+
+
+
 
 
 
