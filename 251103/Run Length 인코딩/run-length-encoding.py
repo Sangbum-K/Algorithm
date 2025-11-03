731b2bd958b548ee1540ea3d0aa4bdf89ab772c9
Sangbum Kim <sangbum0123@naver.com>
2025-11-03 14:58:36 +0900

    Title: Run Length 인코딩, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251103/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,59 @@
+arr = input()
+answer = []
+cnt = 1
+idx = 0
+
+if len(arr) == 1:
+    w = arr[0]
+    answer.append(w)
+    answer.append(cnt)
+
+
+else:
+    for i in range(len(arr)-1):
+        if arr[i] != arr[i+1]:
+
+            w = arr[i]
+            answer.append(w)
+
+            if cnt >= 10:
+                s = cnt // 10
+                r = cnt % 10
+                answer.append(s)
+                answer.append(r)
+            
+            else:
+                answer.append(cnt)
+
+            cnt = 1
+            idx = i
+        
+        else:
+            cnt += 1
+
+    w = arr[idx+1]
+    answer.append(w)
+    cnt = int(len(arr) - idx - 1)
+    if cnt >= 10:
+        s = cnt // 10
+        r = cnt % 10
+        answer.append(s)
+        answer.append(r)
+            
+    else:
+         answer.append(cnt)
+
+
+
+
+    
+
+
+print(len(answer))
+for i in answer:
+    print(i, end = "")
+
+        
+
+
+
