1b985f64198f7e70a7271c479a825cd9bfd3b4ff
Sangbum Kim <sangbum0123@naver.com>
2025-11-03 15:39:37 +0900

    Title: 특정 문자의 유무, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251103/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,13 @@
+str1 = input()
+
+if 'ee' in str1:
+    print('Yes',end = " ")
+
+else :
+    print('No', end = " ")
+
+if 'ab'in str1:
+    print('Yes',end = " ")
+
+else :
+    print('No', end = " ")
