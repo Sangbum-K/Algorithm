039cbbe4029b8047b739f0538d2716c5b6e75c88
Sangbum Kim <sangbum0123@naver.com>
2025-11-04 14:47:46 +0900

    Title: 정수만 추출하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251104/정수만 추출하기/extract-only-integers.py
+++ 251104/정수만 추출하기/extract-only-integers.py
@@ -5,12 +5,18 @@ str1,str2 = input().split()
 for i in range(len(str1)):
     if str1[i].isdigit() == False:
         idx = i
+        break
+    else:
+        idx = len(str1)
 x = int(str1[:idx])
 
 
 for j in range(len(str2)):
     if str2[j].isdigit() == False:
         idx = j
+        break
+    else:
+        idx = len(str2)
 y = int(str2[:idx])
 
 print(x+y)
