d2e5603a2a6eb2e222af47b08aeba4b1db10afcb
Sangbum Kim <sangbum0123@naver.com>
2025-11-07 15:36:40 +0900

    Title: 숫자 차례로 출력하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251107/숫자 차례로 출력하기/to-output-numerically.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+def inc(n):
+    if n == 0:
+        return
+    inc(n-1)
+    print(n,end = " ")
+
+
+def dec(n):
+    if n == 0:
+        return
+    print(n,end = " ")
+    dec(n-1)
+
+inc(n)
+print()
+dec(n)
+    
