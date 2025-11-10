e7c6d75866f79ea1bce8e550689a8d5412be88a8
Sangbum Kim <sangbum0123@naver.com>
2025-11-10 15:06:05 +0900

    Title: 코드네임, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251110/코드네임/code-name.py
@@ -0,0 +1,30 @@
+MAX_N = 5
+
+codenames = []
+scores = []
+for _ in range(MAX_N):
+    codename, score = input().split()
+    codenames.append(codename)
+    scores.append(int(score))
+
+class Agent():
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+
+agents = []
+for i in range(MAX_N):
+    agents.append(Agent(codenames[i], scores[i]))
+
+
+mn = agents[0].score
+idx = 0
+
+for i in range(len(agents)):
+    if agents[i].score < mn:
+        idx = i
+
+
+print(agents[idx].name,agents[idx].score)
+    
+
