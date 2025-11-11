26b7892375b331b186d5973bd3c7d59974feca84
Sangbum Kim <sangbum0123@naver.com>
2025-11-11 16:46:27 +0900

    Title: 줄 세우기 2, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251111/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,11 @@
+n = int(input())
+students = [
+    (h, w, i + 1)
+    for i, (h, w) in enumerate([tuple(map(int, input().split())) for _ in range(n)])
+]
+
+
+students.sort(key=lambda x: (x[0], -x[1]))
+
+for s in students:
+    print(s[0],s[1],s[2])
