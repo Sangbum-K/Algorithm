6fd75fce01d57da5bc8a314eb66e3bed3ec26081
Sangbum Kim <sangbum0123@naver.com>
2025-11-12 14:58:18 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251112/Date to Date/date-to-date.py
+++ 251112/Date to Date/date-to-date.py
@@ -3,20 +3,15 @@ m1, d1, m2, d2 = map(int, input().split())
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
-sumd1 = d1
-for i in range(m1+1):
-    sumd1+=num_of_days[i]
-
+sumd1 = num_of_days[m1] - d1 + 1
 
 sumd2 = d2
-for i in range(m2+1):
+for i in range(m1+1,m2):
     sumd2+=num_of_days[i]
 
+elapsed_days = sumd1+sumd2
 
-elapsed_days = sumd2 - sumd1 
-if m1 == m2:
-    elapsed_days += 1
-if d1 > d2:
-    elapsed_days += 1
+if (m1 == m2):
+    elapsed_days = d2-d1+1
 
 print(elapsed_days)
