5f7fee31008ea69098d6bb827678d02f36bc69d8
Sangbum Kim <sangbum0123@naver.com>
2025-11-12 14:43:45 +0900

    Title: Date to Date, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251112/Date to Date/date-to-date.py
@@ -0,0 +1,16 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+sumd1 = d1
+for i in range(m1+1):
+    sumd1+=num_of_days[i]
+
+
+sumd2 = d2
+for i in range(m2+1):
+    sumd2+=num_of_days[i]
+
+elapsed_days = sumd2 - sumd1
+print(elapsed_days)
