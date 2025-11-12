4ac69910f0b57b74cc9e2364f77cbe4a81d3cfed
Sangbum Kim <sangbum0123@naver.com>
2025-11-12 15:28:35 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251112/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,33 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+
+
+def num_of_days(m, d):
+
+    days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+
+    for i in range(1, m):
+        total_days += days[i]
+    
+    total_days += d
+    
+    return total_days    
+
+total_days = num_of_days(m2, d2) - num_of_days(m1, d1) 
+
+r = total_days % 7
+
+week = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+
+cnt = 0
+
+
+for i in range(total_days+1):
+    w = week[i%7]
+    if w == A:
+
+        cnt+=1
+
+print(cnt)
