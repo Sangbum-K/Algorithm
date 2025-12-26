a2d5b4173b625d31b89592bd6ac2d5ead12589b9
Sangbum Kim <sangbum0123@naver.com>
2025-12-26 17:22:15 +0900

    Title: 오목, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251226/오목/O-mok.py
+++ 251226/오목/O-mok.py
@@ -3,8 +3,11 @@ board = [list(map(int, input().split())) for _ in range(19)]
 answer = 0
 answer_point = (0,0)
 
-for i in range(2,17):
-    for j in range(2,17):
+def in_range(x,y):
+    return 0 <= x and x < 19 and 0 <= y and y < 19
+
+for i in range(0,19):
+    for j in range(0,19):
 
         if board[i][j] != 0:
             color = board[i][j]
@@ -12,7 +15,7 @@ for i in range(2,17):
 
             cnt = 0
             for x in range(i-2,i+3):
-                if board[x][j] == color:
+                if in_range(x,j) and board[x][j] == color:
                     cnt += 1
             
             if cnt == 5:
@@ -21,7 +24,7 @@ for i in range(2,17):
         
             cnt = 0
             for y in range(j-2,j+3):
-                if board[i][y] == color:
+                if in_range(i,y) and board[i][y] == color:
                     cnt += 1
 
             if cnt == 5:
@@ -31,7 +34,7 @@ for i in range(2,17):
 
             cnt = 0
             for z1 in range(-2,3):
-                if board[i+z1][j+z1] == color:
+                if in_range(i+z1,j+z1) and board[i+z1][j+z1] == color:
                     cnt += 1
             if cnt == 5:
                 answer = color
@@ -39,7 +42,7 @@ for i in range(2,17):
 
             cnt = 0
             for z2 in range(-2,3):
-                if board[i+z2][j-z2] == color:
+                if in_range(i+z2,j-z2) and board[i+z2][j-z2] == color:
                     cnt += 1
             if cnt == 5:
                 answer = color
