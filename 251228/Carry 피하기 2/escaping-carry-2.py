2cfb721245ea110c41c5b5052af5305693c331ac
Sangbum Kim <sangbum0123@naver.com>
2025-12-28 15:16:59 +0900

    Title: Carry 피하기 2, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 251228/Carry 피하기 2/escaping-carry-2.py
+++ 251228/Carry 피하기 2/escaping-carry-2.py
@@ -2,9 +2,9 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 answer = []
-for i in range(0,n-2):
-    for j in range(i+1,n-1):
-        for k in range(j+1,n):
+for i in range(0, n - 2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
             n1 = list(str(arr[i]))
             n1 = n1[::-1]
 
@@ -14,38 +14,38 @@ for i in range(0,n-2):
             n3 = list(str(arr[k]))
             n3 = n3[::-1]
 
-            maxlen = max((len(n1),len(n2),len(n3)))
+            maxlen = max((len(n1), len(n2), len(n3)))
 
-            for n in range(maxlen-len(n1)):
+            for _ in range(maxlen - len(n1)):
                 n1.append('0')
-            for n in range(maxlen-len(n2)):
+            for _ in range(maxlen - len(n2)):
                 n2.append('0')
-            for n in range(maxlen-len(n3)):
+            for _ in range(maxlen - len(n3)):
                 n3.append('0')
-            
+
+
             carry = 0
 
             for m in range(maxlen):
-                if(int(n1[m])+int(n2[m])+int(n3[m]) >= 10):
+                if (int(n1[m]) + int(n2[m]) + int(n3[m]) >= 10):
                     carry = 1
-            
-            
+
             if carry == 0:
                 tmp = []
                 for m in range(maxlen):
-                    tmp.append(int(n1[m])+int(n2[m])+int(n3[m]))
+                    tmp.append(int(n1[m]) + int(n2[m]) + int(n3[m]))
                 tmp = tmp[::-1]
                 answer.append(tmp)
 
-maxx = 0
+maxx = -1
 for num in answer:
-    k = int("".join(map(str,num)))
+    k = int("".join(map(str, num)))
     if k > maxx:
         maxx = k
-print(k)
+print(maxx)
 
 
-              
+
 
 
 
