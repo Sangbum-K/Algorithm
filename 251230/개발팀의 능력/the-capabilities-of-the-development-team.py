bdd211385dd5f8efdf45816556fd14469873df54
Sangbum Kim <sangbum0123@naver.com>
2025-12-30 01:42:18 +0900

    Title: 개발팀의 능력, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251230/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,40 @@
+import sys
+
+ability = list(map(int, input().split()))
+
+ans = sys.maxsize
+
+
+for i in range(len(ability)-1):
+    for j in range(i+1,len(ability)):
+
+        team1 = ability[i] + ability[j]
+
+        for n in range(len(ability)-1):
+            for m in range(n+1,len(ability)):
+                if n ==i or n ==j or m == i or m == j:
+                    continue
+
+
+                team2 = ability[n]+ability[m]
+
+                team3 = sum(ability)-team1-team2
+                if team1 == team2 or team1 == team3 or team2 == team3:
+                    continue
+
+                Max_val = max(team1,team2,team3)
+                min_val = min(team1,team2,team3)
+                diff = abs(Max_val-min_val)
+
+
+                ans = min(ans,diff)
+
+if ans == 0:
+    ans = -1
+print(ans)
+                
+
+
+                
+
+
