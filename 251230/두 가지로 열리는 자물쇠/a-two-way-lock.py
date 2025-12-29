83b23871eb23f8106c68cb52b91e668f24004f0c
Sangbum Kim <sangbum0123@naver.com>
2025-12-30 00:49:10 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251230/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,37 @@
+N = int(input())
+com1= list(map(int, input().split()))
+com2= list(map(int, input().split()))
+
+
+num = [i for i in range(1,N+1)]
+
+
+L1 = []
+for i in com1:
+    l1 = []
+    for j in range(-2,3):
+        n = i + j
+        if n <= 0:
+            n = N + n
+        l1.append(n)
+    L1.append(l1)
+
+
+L2 = []
+for i in com2:
+    l2 = []
+    for j in range(-2,3):
+        n = i + j
+        if n <= 0:
+            n = N + n
+        l2.append(n)
+    L2.append(l2)
+
+ans = 1
+for n1,n2 in zip(L1,L2):
+    ans *= len(set(n1)&set(n2))
+
+print(250-ans)
+
+    
+        
