ff7917860485e092624728806be576a4f6df60fa
Sangbum Kim <sangbum0123@naver.com>
2026-01-01 23:14:41 +0900

    Title: 겹치지 않는 선분 2, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260101/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 260101/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -1,13 +1,20 @@
 n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
 
-cnt = 0
+ans = 0
 
-for i in range(n-1):
-    for j in range(i+1,n):
+for i in range(n):
+    cnt = 0
+
+    for j in range(n):
+        if i == j :
+            continue
+        
         if (lines[i][0]-lines[j][0]) * (lines[i][1]-lines[j][1]) < 0:
-
             cnt += 1
+    
+    if cnt == 0:
+        ans += 1
+        
 
-ans = n - cnt*2
 print(ans)
