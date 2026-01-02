e1a9ec307c743d865ea5a87392cd727d1d583f13
Sangbum Kim <sangbum0123@naver.com>
2026-01-02 16:59:46 +0900

    Title: 데이터센터의 온도 조정 2, Time: 54ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260102/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,21 @@
+N, C, G, H = map(int, input().split())
+ranges = [tuple(map(int, input().split())) for _ in range(N)]
+start = min(item[0] for item in ranges)
+end = max(item[1] for item in ranges)
+
+ans = 0
+
+for temp in range(start,end + 1):
+    efficiency = 0
+    for i,j in ranges:
+        if temp < i:
+            efficiency += C
+        elif i <= temp and temp <= j:
+            efficiency += G
+        elif j < temp:
+            efficiency += H
+
+
+    ans = max(ans,efficiency)
+print(ans)
+
